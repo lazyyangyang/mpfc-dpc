e8b1f6f030be4d00f2cf71ff499d9f4b6d0e1afe
lazyyangyang <1549335703@qq.com>
2026-08-09 23:10:26 +0800

    Add generation commands for four datasets

--- sh/ufc_cifar.sh
+++ sh/ufc_cifar.sh
@@ -1,61 +1,82 @@
 #!/bin/bash
+set -e
 
 export CUDA_VISIBLE_DEVICES=0
 
-# CIFAR-100 example
+# CIFAR-10
 python ufc_generation/ufc_cda_cifar_sam_dire.py \
-    --iteration 1000 \
-    --r-bn 1 \
-    --batch-size 100 \
-    --lr 0.25 \
-    --exp-name generated_results \
-    --wandb-name cifar100-ipc10-mpfc-dpc \
-    --store-best-images \
-    --syn-data-path syn/ \
-    --init_path init_images/c100/ \
-    --ipc 10 \
-    --dataset cifar100
-
-# CIFAR-10 example
-python ufc_generation/ufc_cda_cifar_sam_dire.py \
-    --iteration 1000 \
+    --iteration 1500 \
     --r-bn 1 \
     --batch-size 10 \
     --lr 0.25 \
+    --ipc 10 \
     --exp-name generated_results \
-    --wandb-name cifar10-ipc10-mpfc-dpc \
+    --wandb-name cifar10-ipc10 \
     --store-best-images \
     --syn-data-path syn/ \
     --init_path init_images/c10/ \
-    --ipc 10 \
-    --dataset cifar10
+    --dataset cifar10 \
+    --easy2hard-mode step \
+    --milestone 0.65 \
+    --rcd 1.0 \
+    --rcdm 1.0 \
+    --redm 0.5
 
-# Tiny-ImageNet example
-python ufc_generation/ufc_cda_tiny_sam_dire.py \
-    --iteration 1000 \
+# CIFAR-100
+python ufc_generation/ufc_cda_cifar_sam_dire.py \
+    --iteration 1500 \
     --r-bn 1 \
     --batch-size 100 \
     --lr 0.25 \
+    --ipc 10 \
     --exp-name generated_results \
-    --wandb-name tiny-ipc10-mpfc-dpc \
+    --wandb-name cifar100-ipc10 \
+    --store-best-images \
+    --syn-data-path syn/ \
+    --init_path init_images/c100/ \
+    --dataset cifar100 \
+    --easy2hard-mode step \
+    --milestone 0.65 \
+    --rcd 1.0 \
+    --rcdm 1.0 \
+    --redm 0.5
+
+# Tiny-ImageNet
+python ufc_generation/ufc_cda_tiny_sam_dire.py \
+    --iteration 1500 \
+    --r-bn 1 \
+    --batch-size 100 \
+    --lr 0.25 \
+    --ipc 10 \
+    --exp-name generated_results \
+    --wandb-name tiny-ipc10 \
     --store-best-images \
     --syn-data-path syn/ \
     --init_path init_images/tiny/ \
-    --ipc 10 \
-    --dataset tiny
+    --dataset tiny \
+    --easy2hard-mode step \
+    --milestone 0.65 \
+    --rcd 1.0 \
+    --rcdm 1.0 \
+    --redm 0.5
 
-# ImageNet-100 example
+# ImageNet-100
 python ufc_generation/ufc_cda_imagenet100_sam_dire.py \
-    --iteration 1000 \
+    --iteration 1500 \
     --r-bn 1 \
     --batch-size 20 \
     --lr 0.1 \
+    --ipc 10 \
     --exp-name generated_results \
-    --wandb-name imagenet100-ipc10-mpfc-dpc \
+    --wandb-name imagenet100-ipc10 \
     --store-best-images \
     --syn-data-path syn/ \
     --init_path init_images/tiny/ \
     --class-list data/wnids.txt \
-    --ipc 10 \
     --dataset imagenet100 \
-    --num-class 100
+    --num-class 100 \
+    --easy2hard-mode step \
+    --milestone 0.65 \
+    --rcd 1.0 \
+    --rcdm 1.0 \
+    --redm 0.5
